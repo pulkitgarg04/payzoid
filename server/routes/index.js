3e0ae2dfba9b50d9cc7e4ee9e0bf6852dc6a7d99
Pulkit Garg <pulkitgargbnl@gmail.com>
2024-10-10 23:53:25 +0530

    test backend for production

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,9 @@ import userRouter from './user.js';
 import accountRouter from './account.js';
 const router = express.Router();
 
+router.use('/', (req, res) => {
+  res.send('Welcome to the PayZoid API');
+});
 router.use('/user', userRouter);
 router.use('/account', accountRouter);
 
